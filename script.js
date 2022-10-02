fdd46cf44d6f38922bb31b329f6b708815ac9528
Viktoriia <korvik8098@gmail.com>
2022-10-02 15:41:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Перше завдання
+const name = prompt('Enter your name');
+const result = 'Hello, ' + name + '! How are you?';
+alert(result);
+
+// Друге завдання
+const day = prompt('What date were you born?');
+const month = prompt('What month were you born? (number)');
+const year = prompt('What year were you born?');
+const so = ('So you were born')
+alert(`${so} ${day}.${month}.${year}!`);
+
+// Третє завдання
+const number = ('1' + ' 2'+ ' 3'+ ' 4'+ ' 5');
+
+
+
+
+
+
+
+
+
